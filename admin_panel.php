d897ba75d6ea9dac8c4325b89a0c1176a1f7734d
poojapauskar <git.poojapauskar@gmail.com>
2016-10-27 13:10:47 +0530

    grey nav bar 3

--- admin_panel.php
+++ admin_panel.php
@@ -19,6 +19,11 @@
 <link rel="stylesheet" href="css/style.css">
 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
 
+<style>
+p{
+  color:white;
+}
+</style>
 </head>
 <body ng-app="" style="overflow-x:hidden">
 
@@ -58,28 +63,28 @@
     </div>
     <div class="col-sm-2">
       <a href="enquiry.php">
-      <button style="width:120px;height:120px" class="mdl-button mdl-js-button mdl-button--raised">
+      <button style="background-color:#607D8B;width:120px;height:120px" class="mdl-button mdl-js-button mdl-button--raised">
       <p>Enquiry</p>
       </button>
       </a>
     </div>
     <div class="col-sm-2">
       <a href="test_ride.php">
-      <button style="width:120px;height:120px" class="mdl-button mdl-js-button mdl-button--raised">
+      <button style="background-color:#607D8B;width:120px;height:120px" class="mdl-button mdl-js-button mdl-button--raised">
       <p>Test Rides</p>
       </button>
       </a>
     </div>
     <div class="col-sm-2">
        <a href="bookings.php">
-       <button style="width:120px;height:120px" class="mdl-button mdl-js-button mdl-button--raised">
+       <button style="background-color:#607D8B;width:120px;height:120px" class="mdl-button mdl-js-button mdl-button--raised">
        <p>bookings</p>
        </button>
        </a>
     </div>
     <div class="col-sm-2">
        <a href="finance.php">
-       <button style="width:120px;height:120px" class="mdl-button mdl-js-button mdl-button--raised">
+       <button style="background-color:#607D8B;width:120px;height:120px" class="mdl-button mdl-js-button mdl-button--raised">
        <p>Finance Requests</p>
        </button>
        </a>
@@ -93,28 +98,28 @@
     </div>
     <div class="col-sm-2">
       <a href="insurance.php">
-      <button style="width:120px;height:120px" class="mdl-button mdl-js-button mdl-button--raised">
+      <button style="background-color:#607D8B;width:120px;height:120px" class="mdl-button mdl-js-button mdl-button--raised">
       <p>Insurance Renewal</p>
       </button>
       </a>
     </div>
     <div class="col-sm-2">
       <a href="service_requests.php">
-      <button style="width:120px;height:120px" class="mdl-button mdl-js-button mdl-button--raised">
+      <button style="background-color:#607D8B;width:120px;height:120px" class="mdl-button mdl-js-button mdl-button--raised">
       <p>Service Requests</p>
       </button>
       </a>
     </div>
     <div class="col-sm-2">
        <a href="inventory.php">
-       <button style="width:120px;height:120px" class="mdl-button mdl-js-button mdl-button--raised">
+       <button style="background-color:#607D8B;width:120px;height:120px" class="mdl-button mdl-js-button mdl-button--raised">
        <p>Inventory</p>
        </button>
        </a>
     </div>
     <div class="col-sm-2">
        <a href="customer_database.php">
-       <button style="width:120px;height:120px" class="mdl-button mdl-js-button mdl-button--raised">
+       <button style="background-color:#607D8B;width:120px;height:120px" class="mdl-button mdl-js-button mdl-button--raised">
        <p>Customer Database</p>
        </button>
        </a>
@@ -129,14 +134,14 @@
     </div>
     <div class="col-sm-2">
       <a href="push_notification.php">
-      <button style="width:120px;height:120px" class="mdl-button mdl-js-button mdl-button--raised">
+      <button style="background-color:#607D8B;width:120px;height:120px" class="mdl-button mdl-js-button mdl-button--raised">
       <p>Mobile App</p>
       </button>
       </a>
     </div>
     <div class="col-sm-2">
       <a href="web_app_user_list.php">
-      <button style="width:120px;height:120px" class="mdl-button mdl-js-button mdl-button--raised">
+      <button style="background-color:#607D8B;width:120px;height:120px" class="mdl-button mdl-js-button mdl-button--raised">
       <p>Admin</p>
       </button>
       </a>
