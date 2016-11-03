1529a026d15771f8341bc827f37a8c9479788ad1
poojapauskar <git.poojapauskar@gmail.com>
2016-11-03 13:30:41 +0530

    live apis added

--- web_app_user_list.php
+++ web_app_user_list.php
@@ -364,7 +364,7 @@ $employees_details_info = json_decode($output_data,true);
             <?php 
                 for ($x = 0; $x <= $employees_details_info['count']/10; $x++) { ?>
                     <td>
-                      <form method="get" action="service_requests.php">
+                      <form method="get" action="web_app_user_list.php">
                         <input type="hidden" name="page_no" value=<?php echo $x+1 ?>>
                         <button type="submit"><?php echo $x+1 ?></button>
                       </form>
